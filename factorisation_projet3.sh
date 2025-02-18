caa04d865f23a62b8b3b042d3e835dbace36851d
fgaigy <fgaigy.acc@gmail.com>
2025-02-19 00:13:45 +0300

    Creation de la fonction factorisation_naive()

--- factorisation_projet3.sh
+++ factorisation_projet3.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+echo "***Factorisation***"
+
+
+function factorisation_naive()
+{
+    a=$1
+    b=2
+    until [ $(($a/$b)) -lt $b ]; do
+        if [ $((a%b)) -eq 0 ]; then
+            echo " ==> $n = $b * $(($a/$b))"
+            exit 0
+        fi
+        b=$(($b+1))
+    done
+    echo "$n est un nombre premier."
+}
+
+read -p "Entrez un nombre entier: " n
+factorisation_naive $n
